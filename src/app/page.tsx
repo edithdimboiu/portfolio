65bec39aafa9ef0d4ca7374914df332e3a44b038
Edith Dimboiu <edith@dimboiu.com>
2024-05-02 16:33:58 +0200

    add DaisyUI dependency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
+import './global.css'
+
 
 export default function Home() {
   return (
-    <h1>Edith Dîmboiu - Personal portfolio</h1>
+    <div className="diff aspect-[16/9]">
+    <div className="diff-item-1">
+      <div className="bg-primary text-primary-content text-9xl font-black grid place-content-center">Hello</div>
+    </div>
+    <div className="diff-item-2">
+      <div className="bg-base-200 text-9xl font-black grid place-content-center">Hello</div>
+    </div>
+    <div className="diff-resizer"></div>
+  </div>
   );
 }
