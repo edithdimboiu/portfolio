b702bae3b2db3dec70cfb7978b48a9a7e810a78f
Edith Dimboiu <edith@dimboiu.com>
2024-08-03 17:09:45 +0200

    create Navbar component

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="pt-2  mx-auto w-full fixed mt-0  z-10 top-0 bg-orange-50">
+      <div className="navbar px-8  mx-auto flex-col place-content-between sm:flex-row">
+        <Link href="/" className="">
+          <h1 className="lg:text-8xl  text-6xl">ED</h1>
+        </Link>
+        <ul className="menu menu-horizontal justify-end md:ml-8">
+          <li className="capitalize text-xl sm:text-2xl">
+            <Link href="">Portfolio</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="divider divider-primary my-0"></div>
+      <div className="divider divider-secondary my-0"></div>
+      <div className="divider divider-accent my-0"></div>
+    </nav>
+  );
+};
+export default Navbar;
