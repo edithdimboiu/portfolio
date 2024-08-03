227e882ce7ffe96f405c65e5974eeef3fe39a619
Edith Dimboiu <edith@dimboiu.com>
2024-08-03 17:18:43 +0200

    add homepage hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,25 @@
-import './global.css'
-
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="diff aspect-[16/9]">
-    <div className="diff-item-1">
-      <div className="bg-primary text-primary-content text-9xl font-black grid place-content-center">Hello</div>
+    <div className="hero">
+      <div className="hero-content text-center">
+        <div className="max-w-max">
+          <h1 className="text-3xl font-bold">Hey there!</h1>
+          <p className="py-6 text-l md:text-2xl">
+            I'm Edith, a passionate Fronted Web Developer, who currently lives in Switzerland. After building my career in accounting and taxes I decided to face another challenge and switch careers to Frontend Web Development. I always loved solving problems and optimising, so I feel that it fits
+            me like a glove. I am open to new challenges and opportunities. Below you can see some of my projects and if you find something catchy please feel free to contact/follow me on{" "}
+            <Link className="text-primary" href="https://linkedin.com/in/edith-dimboiu-1670b869" target="_blank" rel="noopener noreferrer">
+              Linkedin
+            </Link>{" "}
+            or{" "}
+            <Link className="text-primary" href="https://github.com/edithdimboiu" target="_blank" rel="noopener noreferrer">
+              Github
+            </Link>
+            .
+          </p>
+        </div>
+      </div>
     </div>
-    <div className="diff-item-2">
-      <div className="bg-base-200 text-9xl font-black grid place-content-center">Hello</div>
-    </div>
-    <div className="diff-resizer"></div>
-  </div>
   );
 }
