f1b0bc3152c8dcf72a519eb5626a6c80717eb553
Edith Dimboiu <edith@dimboiu.com>
2024-08-07 01:49:09 +0200

    Refactor personal library page and portfolio page  for cleaner code and use ProjectHeader component

--- src/app/personal-library/page.tsx
+++ src/app/personal-library/page.tsx
@@ -1,5 +1,18 @@
-import Link from "next/link";
 import Carousel from "../components/Carousel";
+import ProjectHeader from "../components/ProjectHeader";
+
+const links = [
+  {
+    href: "https://personal-library.edith.info",
+    buttonClass: "btn btn-primary",
+    text: "Live Demo",
+  },
+  {
+    href: "https://github.com/edithdimboiu/PersonalLibrary.git",
+    buttonClass: "btn btn-secondary",
+    text: "Code",
+  },
+];
 
 const slides: { id: string; src: string; type: "image" | "video" }[] = [
   { id: "slide1", src: "/assets/slide1.jpg", type: "image" },
@@ -10,75 +23,94 @@ const slides: { id: string; src: string; type: "image" | "video" }[] = [
 
 const MyLibraryProject: React.FC = () => {
   return (
-    <div className="custom-heading">
-      <header className="flex my-8 justify-between flex-col md:flex-row gap-y-8 items-center">
-        <h2 className="text-2xl md:text-3xl">Personal Library</h2>
-        <ul className="inline-flex gap-4">
-          <li>
-            <Link href="https://personal-library.edith.info" target="_blank" rel="noopener noreferrer">
-              {" "}
-              <button className="btn btn-primary">Live Demo</button>
-            </Link>
-          </li>
-          <li>
-            <Link href="https://github.com/edithdimboiu/PersonalLibrary.git" target="_blank" rel="noopener noreferrer">
-              <button className="btn btn-secondary">Code</button>
-            </Link>{" "}
-          </li>
-        </ul>
-      </header>
+    <div>
+      <ProjectHeader title="Personal Library" links={links} />
 
       <section>
         <Carousel slides={slides} />
       </section>
-
-      <main>
-        <section>
-          <h3>How it all started</h3>
-          <p>The PersonalLibrary app was designed for readers who want to track their reading habits and enjoy detailed statistics about their collections.</p>
-        </section>
-
-        <section>
-          <h3>Features</h3>
-          <p>This web-based app enables users to search for books and add them to their online libraries, helping them keep track of their purchases and avoid buying duplicate titles.</p>
-        </section>
-
-        <section>
-          <h3>User Interaction</h3>
-          <p>Readers can mark books as read or unread, with the app generating insightful statistics, including the number of books and pages read, as well as the total amount spent on books.</p>
-        </section>
-
-        <section>
-          <h3>Data and Accuracy</h3>
-          <p>By utilizing data from the Google Books API, MyLibrary ensures that users have access to accurate and up-to-date information.</p>
-        </section>
-
-        <section>
-          <h3>Technologies/Frameworks/Libraries</h3>
-          <ul>
-            <li>Javascript</li>
-            <li>ReactJS</li>
-            <li>React Testing Library</li>
-            <li>Jest</li>
-            <li>Git</li>
-          </ul>
-        </section>
-
-        <section>
-          <h3>Challenges and Solutions</h3>
-          <p>
-            One of the biggest challenges I faced in this project was state management. Initially, I managed state directly within individual components, but as the application became more complex, I realized that this approach was not scalable and led to difficult to maintain code. Next, I tried a
-            "lifting state up" approach, centralizing state in parent components and passing data through props. While this improved state management to some extent, it introduced additional complexity and made the code structure hard to follow. Ultimately, I decided to implement React's Context API
-            to handle global state management. This allowed me to efficiently share data and functionalities between components while keeping the code clean and organized. My solution was the following:
-          </p>
-        </section>
-
-        <section>
-          <h3>Future Improvements</h3>
-          <p>As the PersonalLibrary App was designed as a frontend app, the data is saved into localstorage. As a future improvement, I would consider creating user accounts and saving the data into a database.</p>
-        </section>
-      </main>
+      <PersonalLibraryDescription />
     </div>
   );
 };
+
+const PersonalLibraryDescription = () => {
+  return (
+    <main>
+      <section>
+        <h3>How it all started</h3>
+        <p>
+          The PersonalLibrary app was designed for readers who want to track
+          their reading habits and enjoy detailed statistics about their
+          collections.
+        </p>
+      </section>
+
+      <section>
+        <h3>Features</h3>
+        <p>
+          This web-based app enables users to search for books and add them to
+          their online libraries, helping them keep track of their purchases and
+          avoid buying duplicate titles.
+        </p>
+      </section>
+
+      <section>
+        <h3>User Interaction</h3>
+        <p>
+          Readers can mark books as read or unread, with the app generating
+          insightful statistics, including the number of books and pages read,
+          as well as the total amount spent on books.
+        </p>
+      </section>
+
+      <section>
+        <h3>Data and Accuracy</h3>
+        <p>
+          By utilizing data from the Google Books API, MyLibrary ensures that
+          users have access to accurate and up-to-date information.
+        </p>
+      </section>
+
+      <section>
+        <h3>Technologies/Frameworks/Libraries</h3>
+        <ul>
+          <li>Javascript</li>
+          <li>ReactJS</li>
+          <li>React Testing Library</li>
+          <li>Jest</li>
+          <li>Git</li>
+        </ul>
+      </section>
+
+      <section>
+        <h3>Challenges and Solutions</h3>
+        <p>
+          One of the biggest challenges I faced in this project was state
+          management. Initially, I managed state directly within individual
+          components, but as the application became more complex, I realized
+          that this approach was not scalable and led to difficult to maintain
+          code. Next, I tried a "lifting state up" approach, centralizing state
+          in parent components and passing data through props. While this
+          improved state management to some extent, it introduced additional
+          complexity and made the code structure hard to follow. Ultimately, I
+          decided to implement React's Context API to handle global state
+          management. This allowed me to efficiently share data and
+          functionalities between components while keeping the code clean and
+          organized. My solution was the following:
+        </p>
+      </section>
+
+      <section>
+        <h3>Future Improvements</h3>
+        <p>
+          As the PersonalLibrary App was designed as a frontend app, the data is
+          saved into localstorage. As a future improvement, I would consider
+          creating user accounts and saving the data into a database.
+        </p>
+      </section>
+    </main>
+  );
+};
+
 export default MyLibraryProject;
--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import ProjectHeader from "../components/ProjectHeader";
 import Carousel from "../components/Carousel";
 
 const slides: { id: string; src: string; type: "image" | "video" }[] = [
@@ -7,67 +7,94 @@ const slides: { id: string; src: string; type: "image" | "video" }[] = [
   { id: "slide3", src: "/assets/slide6.jpg", type: "image" },
   { id: "slide4", src: "/videos/portfolio.mov", type: "video" },
 ];
+const links = [
+  {
+    href: "https://github.com/edithdimboiu/portfolio.git",
+    buttonClass: "btn btn-secondary",
+    text: "Code",
+  },
+];
 
 const PortfolioProject: React.FC = () => {
   return (
-    <div className="custom-heading">
-      <header className="flex my-8 justify-between flex-col md:flex-row gap-y-8 items-center">
-        <h2 className="text-2xl md:text-3xl">Portfolio</h2>
-        <ul className="inline-flex gap-4">
-          <Link href="https://github.com/edithdimboiu/portfolio.git" target="_blank" rel="noopener noreferrer">
-            <button className="btn btn-secondary">Code</button>
-          </Link>
-        </ul>
-      </header>
+    <div>
+      <ProjectHeader title="Portfolio" links={links} />
 
       <section>
         <Carousel slides={slides} />
       </section>
-
-      <main>
-        <section>
-          <h3>How it all started</h3>
-          <p>The portfolio app was created to showcase my skills and projects as a frontend developer. It serves as a playground for experimenting with new technologies and frameworks, specifically Next.js, and TypeScript.</p>
-        </section>
-
-        <section>
-          <h3>Features</h3>
-          <p>This web-based portfolio app presents my projects, skills, and contact information in an organized and visually appealing manner. It demonstrates responsive design principles and modern frontend development techniques.</p>
-          <p>Users can browse through my projects, read detailed descriptions, and view pictures/videos in order to understand better how the apps work.</p>
-        </section>
-
-        <section>
-          <h3>Data and Accuracy</h3>
-          <p>By leveraging Next.js, the app benefits from the file-based routing system, which simplifies navigation and enhances performance. The static site generation capabilities of Next.js ensure fast load times and an efficient user experience.</p>
-        </section>
-
-        <section>
-          <h3>Technologies/Frameworks/Libraries</h3>
-          <ul>
-            <li>Typescript</li>
-            <li>Next</li>
-            <li>Tailwind</li>
-            <li>DaisyUI</li>
-            <li>React Testing Library</li>
-            <li>Jest</li>
-            <li>Git</li>
-          </ul>
-        </section>
-
-        <section>
-          <h3>Challenges and Solutions</h3>
-          <p>
-            One significant challenge I faced was understanding Next.js's routing system and file structure. Initially, adapting to the file-based routing and learning how Next.js handles pages and dynamic routes took some time. However, by studying the documentation and building simple projects to
-            practice, I gradually became comfortable with these concepts.
-          </p>
-        </section>
-
-        <section>
-          <h3>Future Improvements</h3>
-          <p>Adding a blog section.</p>
-        </section>
-      </main>
+      <PortfolioDescription />
     </div>
   );
 };
+
+const PortfolioDescription = () => {
+  return (
+    <main>
+      <section>
+        <h3>How it all started</h3>
+        <p>
+          The portfolio app was created to showcase my skills and projects as a
+          frontend developer. It serves as a playground for experimenting with
+          new technologies and frameworks, specifically Next.js, and TypeScript.
+        </p>
+      </section>
+
+      <section>
+        <h3>Features</h3>
+        <p>
+          This web-based portfolio app presents my projects, skills, and contact
+          information in an organized and visually appealing manner. It
+          demonstrates responsive design principles and modern frontend
+          development techniques.
+        </p>
+        <p>
+          Users can browse through my projects, read detailed descriptions, and
+          view pictures/videos in order to understand better how the apps work.
+        </p>
+      </section>
+
+      <section>
+        <h3>Data and Accuracy</h3>
+        <p>
+          By leveraging Next.js, the app benefits from the file-based routing
+          system, which simplifies navigation and enhances performance. The
+          static site generation capabilities of Next.js ensure fast load times
+          and an efficient user experience.
+        </p>
+      </section>
+
+      <section>
+        <h3>Technologies/Frameworks/Libraries</h3>
+        <ul>
+          <li>Typescript</li>
+          <li>Next</li>
+          <li>Tailwind</li>
+          <li>DaisyUI</li>
+          <li>React Testing Library</li>
+          <li>Jest</li>
+          <li>Git</li>
+        </ul>
+      </section>
+
+      <section>
+        <h3>Challenges and Solutions</h3>
+        <p>
+          One significant challenge I faced was understanding Next.js's routing
+          system and file structure. Initially, adapting to the file-based
+          routing and learning how Next.js handles pages and dynamic routes took
+          some time. However, by studying the documentation and building simple
+          projects to practice, I gradually became comfortable with these
+          concepts.
+        </p>
+      </section>
+
+      <section>
+        <h3>Future Improvements</h3>
+        <p>Adding a blog section.</p>
+      </section>
+    </main>
+  );
+};
+
 export default PortfolioProject;
