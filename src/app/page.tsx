72ce5ccd24d22a1856ef2108664a6db0339c2b89
Edith Dimboiu <edith@dimboiu.com>
2025-08-19 15:06:18 +0200

    add Blog button functionality and render the blog section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,23 @@
 "use client";
 import Projects from "./components/Projects";
+import BlogSection from "./components/Blog";
 import { useState, useRef, useEffect } from "react";
 
 export default function Home() {
   const [showProjects, setShowProjects] = useState(false);
+  const [showBlog, setShowBlog] = useState(false);
   const projectsRef = useRef<HTMLDivElement | null>(null);
+  const blogRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     if (showProjects && projectsRef.current) {
       projectsRef.current.scrollIntoView({ behavior: "smooth" });
+    } else if (showBlog && blogRef.current) {
+      blogRef.current.scrollIntoView({ behavior: "smooth" });
     } else if (!showProjects) {
       window.scrollTo({ top: 0, behavior: "smooth" });
     }
-  }, [showProjects]);
+  }, [showProjects, showBlog]);
 
   return (
     <div className="flex flex-col pt-[125px] max-w-screen-xl mx-auto">
@@ -20,7 +25,19 @@ export default function Home() {
         <Hero />
         <div className="relative overflow-hidden pb-8">
           <div className="relative z-10 flex flex-row justify-end items-center gap-4 sm:gap-12">
-            <button className="text-2xl md:text-4xl font-semibold text-primary px-3 sm:px-6 py-2 sm:py-3 border-b-2 border-transparent hover:border-primary transition-transform duration-300 hover:scale-105">
+            <button
+              className={`text-2xl md:text-4xl font-semibold  text-primary px-3 sm:px-6 py-2 sm:py-3 transition flex items-center justify-center gap-2 ${
+                showBlog
+                  ? "border-b-4 border-accent font-bold"
+                  : "border-b-2 border-transparent hover:border-primary"
+              } hover:scale-105 duration-300`}
+              onClick={() => {
+                setShowBlog(!showBlog);
+                setShowProjects(false);
+              }}
+              aria-expanded={showBlog}
+              aria-controls="blog-section"
+            >
               Blog
             </button>
 
@@ -31,15 +48,28 @@ export default function Home() {
                   ? "border-b-4 border-accent text-secondary text-opacity-85 font-bold"
                   : "border-b-2 border-transparent hover:border-secondary border-opacity-65 text-secondary text-opacity-80"
               } hover:scale-105 duration-300`}
-              onClick={() => setShowProjects(!showProjects)}
+              onClick={() => {
+                setShowProjects(!showProjects);
+                setShowBlog(false);
+              }}
               aria-expanded={showProjects}
               aria-controls="projects-section"
             >
-              {showProjects ? "Hide Projects" : "Projects"}
+              Projects
             </button>
           </div>
         </div>
       </section>
+
+      {showBlog && (
+        <section
+          id="blog-section"
+          ref={blogRef}
+          className="pt-[125px] px-0 mb-5 sm:px-16 max-w-screen-xl mx-auto animate-fadeIn"
+        >
+          <BlogSection />
+        </section>
+      )}
 
       {showProjects && (
         <section
